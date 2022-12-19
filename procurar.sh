31fe9eae4a60782d9b3e98f2ca21b5005d835530
EdegarJunior <120976927+EdegarJunior@users.noreply.github.com>
2022-12-19 14:18:30 -0300

    Update procurar.sh

--- procurar.sh
+++ procurar.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 ## Conectar OLT
-function conectar-olt()
-{
-sshpass -p "sua-senha" ssh seu-login@ip-da-olt "$@"
+function conectar-olt() {
+	sshpass -p "sua-senha" ssh seu-login@ip-da-olt "$@"
 }
 ## Converter Maiusculo em Minusculo
-function conversor()
-{
+function conversor() {
 	echo $1 | tr 'A-Z' 'a-z'
 }
 ## Procurar MAC na Barra
-function ProcuraOLT()
-{
-conectar-olt show running-config service-port $(conectar-olt show mac-address-table \| include $1 | tr 'A-Z' 'a-z' | awk '{print $1}' | tr -d '-' | tr -d 'a-z') | awk '{print $2}' | grep "/" > /tmp/mac-pon && conectar-olt show running-config service-port $(conectar-olt show mac-address-table \| include $1 | tr 'A-Z' 'a-z' | awk '{print $1}' | tr -d '-' | tr -d 'a-z') | awk '{print $4}' | grep "[0-9]" > /tmp/mac-onu
+function procura-mac() {
+	conectar-olt show running-config service-port $(conectar-olt show mac-address-table \| include $1 | tr 'A-Z' 'a-z' | awk '{print $1}' | tr -d '-' | tr -d 'a-z') | awk '{print $2}' | grep "/" > /tmp/mac-pon && conectar-olt show running-config service-port $(conectar-olt show mac-address-table \| include $1 | tr 'A-Z' 'a-z' | awk '{print $1}' | tr -d '-' | tr -d 'a-z') | awk '{print $4}' | grep "[0-9]" > /tmp/mac-onu
 }
 ### Execução
-Procura-OLT $(conversor-mac $1); Procura-OLT $(conversor-mac $1); conectar-olt show interface gpon $(cat /tmp/mac-pon) onu $(cat /tmp/mac-onu) | grep -E "Name|Serial|Rx|Operati" && printf "pon " && cat /tmp/mac-pon && printf "onu " && cat /tmp/mac-onu 
+procura-mac $(conversor-mac $1); procura-mac $(conversor-mac $1); conectar-olt show interface gpon $(cat /tmp/mac-pon) onu $(cat /tmp/mac-onu) | grep -E "Name|Serial|Rx|Operati" && printf "pon " && cat /tmp/mac-pon && printf "onu " && cat /tmp/mac-onu 
